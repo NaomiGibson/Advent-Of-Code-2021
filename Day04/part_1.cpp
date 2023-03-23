d3e23dd6991cb8599b331c3d873dca1d7be76a6e
NaomiGibson <128326374+NaomiGibson@users.noreply.github.com>
2023-03-23 00:28:49 +0000

    Update Part 1

--- Day04/part_1.cpp
+++ Day04/part_1.cpp
@@ -1,6 +1,26 @@
 #include "headers.h"
+#include "iostream"
+
+vi rolls;
+vvvi tables;
+
+bool CheckNum(int TableNum, int row, int column, int roll) {
+	int num = tables[TableNum][row][column];
+	if (num == roll) {
+		num = -1;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 std::string part_1(const Input input) {
-	vi rolls = input.first;
-	vvvi boards = input.second;
-}
+	rolls = input.first;
+	tables = input.second;
+
+	std::cout << std::to_string(tables[3][4][2]) << " "
+		<< std::to_string(CheckNum(3, 4, 2, rolls[0])) << " "
+		<< std::to_string(tables[3][4][2]) << " ";
+
+	return "Part 1";
